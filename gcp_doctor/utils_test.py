c8db56b3da9feff29e6eaacdf520787fbdb9ca5f
David Schweikert <dwes@google.com>
2020-12-04 18:21:30 +0100

    improve test coverage for utils.py (fixes: b/173117331)
    Change-Id: Iea732d1783e1d0267d72c01844b21e3c39b3cbec
    GitOrigin-RevId: bf3d9c8d32f79d8655166d61af491ff5c7f6f1d5

--- gcp_doctor/utils_test.py
+++ gcp_doctor/utils_test.py
@@ -1,9 +1,28 @@
 # Lint as: python3
 """Test code in utils.py."""
+from googleapiclient import errors
+
 from gcp_doctor import utils
+
+# https://github.com/googleapis/google-api-python-client/blob/master/tests/test_errors.py
+JSON_ERROR_CONTENT = b"""
+{
+ "error": {
+  "message": "country is required"
+ }
+}
+"""
 
 
 def test_is_region():
   """is_region() should return correct result when presented with region or zone string."""
   assert utils.is_region('us-central1')
   assert not utils.is_region('us-central1-b')
+
+
+def test_http_error_message():
+  """http_error_message extracts properly an error from a Google API JSON string."""
+  error = errors.HttpError(content=JSON_ERROR_CONTENT, resp=None)
+  result = utils.http_error_message(error)
+  expected = 'country is required'
+  assert result == expected
