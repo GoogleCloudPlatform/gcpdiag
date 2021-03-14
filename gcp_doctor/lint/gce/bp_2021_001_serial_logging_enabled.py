8ddbd723519b1fb60364b0ae30082915ab9d983b
Evgenii Ianiuk <eianiuk@google.com>
2021-03-14 14:15:26 +0000

    GCE Lint: Implement 'Serial logging is enabled' test
    Bug: 182684159
    Change-Id: Icc516bd521aa78145ae5a3e3e76dfbdcd016b908

---
+++ gcp_doctor/lint/gce/bp_2021_001_serial_logging_enabled.py
@@ -0,0 +1,30 @@
+# Lint as: python3
+"""Serial port Cloud Logging is enabled.
+
+Serial port output can be often useful for troubleshooting, and enabling serial
+logging makes sure that you don't lose the information when the VM is rebooted.
+
+Reference:
+  https://cloud.google.com/compute/docs/instances/viewing-serial-port-output
+"""
+
+import operator as op
+
+from gcp_doctor import lint, models
+from gcp_doctor.queries import gce
+
+
+def run_test(context: models.Context, report: lint.LintReportTestInterface):
+  instances = gce.get_instances(context)
+  instances_count = 0
+  for i in sorted(instances.values(), key=op.attrgetter('project_id', 'name')):
+    # GKE nodes are checked by another test.
+    if i.is_gke_node():
+      continue
+    instances_count += 1
+    if i.is_serial_port_logging_enabled():
+      report.add_ok(i)
+    else:
+      report.add_failed(i, 'serial port Cloud Logging isn\'t enabled')
+  if not instances_count:
+    report.add_skipped(None, 'no instances found')
--- gcp_doctor/queries/project.py
+++ gcp_doctor/queries/project.py
@@ -1,12 +1,80 @@
 # Lint as: python3
 """Queries related to GCP Projects."""
 
+import functools
 import logging
+import re
+from typing import Dict, Mapping
+
+import googleapiclient.errors
 
 from gcp_doctor import cache, config
 from gcp_doctor.queries import apis
+from gcp_doctor.utils import GcpApiError
 
 diskcache = cache.get_cache()
+
+
+class Project():
+  """Represents a Project resource.
+
+  See also the API documentation:
+  https://cloud.google.com/compute/docs/reference/rest/v1/projects/get
+  """
+  _id: str
+  _metadata: Dict[str, str]
+  _resource_data: dict
+
+  def __str__(self):
+    return self.get_full_path()
+
+  @property
+  def number(self) -> int:
+    return get_project_nr(self._id)
+
+  @property
+  def id(self) -> str:
+    """Project id (not project number)."""
+    return self._id
+
+  @property
+  def name(self) -> str:
+    return self._resource_data['name']
+
+  @property
+  def gce_metadata(self) -> Mapping[str, str]:
+    mapped_metadata: Dict[str, str] = {}
+
+    metadata = self._resource_data.get('commonInstanceMetadata')
+    if metadata and 'items' in metadata:
+      for m_item in metadata['items']:
+        mapped_metadata[m_item.get('key')] = m_item.get('value')
+    return mapped_metadata
+
+  def get_full_path(self) -> str:
+    result = re.match(r'https://www.googleapis.com/compute/v1/(.*)',
+                      self._resource_data['selfLink'])
+    if result:
+      return result.group(1)
+    else:
+      return '>> ' + self._resource_data['selfLink']
+
+  def get_short_path(self) -> str:
+    path = self._id + '/' + self.name
+    return path
+
+  def __init__(self, project_id):
+    gce_api = apis.get_api('compute', 'v1')
+    query = gce_api.projects().get(project=project_id)
+    logging.info('fetching data for project %s', project_id)
+
+    try:
+      response = query.execute(num_retries=config.API_RETRIES)
+    except googleapiclient.errors.HttpError as err:
+      raise GcpApiError(err) from err
+
+    self._id = project_id
+    self._resource_data = response
 
 
 # Project IDs and numbers can't be changed, so use the disk cache.
@@ -20,3 +88,8 @@ def get_project_nr(project_id: str) -> int:
     return response['projectNumber']
   else:
     raise ValueError(f'unknown project: {project_id}')
+
+
+@functools.lru_cache(maxsize=None)
+def get_project(project_id: str) -> Project:
+  return Project(project_id=project_id)
