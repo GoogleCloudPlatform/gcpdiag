9a5a52c6fbe97aa8aa986df8aa67a76da5d65464
David Schweikert <dwes@google.com>
2021-04-15 21:24:10 +0200

    test kokoro x20 upload (b/184159266)
    Change-Id: I6fbe953e940031207ba96e37382ebc3ccb54ab0d

--- kokoro/gcp_ubuntu/release/release.sh
+++ kokoro/gcp_ubuntu/release/release.sh
@@ -9,24 +9,5 @@ cd "${KOKORO_ARTIFACTS_DIR}/git/gcp-doctor"
 pipenv-dockerized run pipenv install --dev
 pipenv-dockerized run make test
 pipenv-dockerized run make coverage-report
-pipenv-dockerized run make kokoro-publish-release
-#
-#echo $KOKORO_KEYSTORE_DIR
-#ls -lR $KOKORO_KEYSTORE_DIR
-
-echo "FOOBAR" >dist/gcp-doctor-0.8
-echo "FOOBAR" >dist/gcp-doctor-0.8-test
-echo "BAZBAZ" >dist/gcp-doctor-0.9-test
-chmod +x dist/gcp-doctor-0.9-test
-ln -s gcp-doctor-0.9-test dist/gcp-doctor-0.9-symlink
-mkdir dist/testdir
-echo "BLABLA" >dist/testdir/blabla
-ln -s blabla dist/testdir/blabla-link
-
-# make sure that the files are not group-writable, because
-# otherwise all mdb/gcp-doctor-users would be allowed, and this
-# is not permitted (more than 500 users)
-chmod -R go-w dist
-
-
-ls -lR ${KOKORO_ARTIFACTS_DIR}
+pipenv-dockerized run make kokoro-bump-release
+pipenv-dockerized run make kokoro-build
