13d87238ecde3c73b2fef923b2e287e6c920fb8e
David Schweikert <dwes@google.com>
2021-04-16 13:50:42 +0200

    Bump version: 0.14 ? 0.15-test
    Change-Id: I2c76bd02fdf2e9d66cdae03443e9a285c4f1ba56
    GitOrigin-RevId: 809623b9f7928c1e885ba2d5c16c9670ce6a1b47

--- gcp_doctor/config.py
+++ gcp_doctor/config.py
@@ -4,7 +4,7 @@
 import appdirs
 
 # gcp-doctor version (not configurable, but useful to have here)
-VERSION = '0.14'
+VERSION = '0.15-test'
 
 # Default number of retries for API Calls.
 API_RETRIES = 2
