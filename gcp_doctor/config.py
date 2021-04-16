5064a90d4070d58d07fa7a61969ba2101c507990
David Schweikert <dwes@google.com>
2021-04-16 07:30:54 +0000

    Merge changes I8d8c54ab,I55e7af25
    * changes:
      release process fixes (b/184159266)
      release process fixes (b/184159266)
    
    GitOrigin-RevId: b032dd37fc94ccf4e0caade7924e8e55aabb0674

--- gcp_doctor/config.py
+++ gcp_doctor/config.py
@@ -4,7 +4,7 @@
 import appdirs
 
 # gcp-doctor version (not configurable, but useful to have here)
-VERSION = '0.10-test'
+VERSION = '0.11-test'
 
 # Default number of retries for API Calls.
 API_RETRIES = 2
