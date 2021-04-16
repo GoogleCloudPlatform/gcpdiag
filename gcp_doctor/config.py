2f1b1b2f1e19bb05bc735388de653043c92f466f
David Schweikert <dwes@google.com>
2021-04-16 11:39:34 +0200

    Bump version: 0.12-test → 0.12
    Change-Id: I219198abedecb0a023617a5b35dbbbef3181da66

--- gcp_doctor/config.py
+++ gcp_doctor/config.py
@@ -4,7 +4,7 @@
 import appdirs
 
 # gcp-doctor version (not configurable, but useful to have here)
-VERSION = '0.12-test'
+VERSION = '0.12'
 
 # Default number of retries for API Calls.
 API_RETRIES = 2
