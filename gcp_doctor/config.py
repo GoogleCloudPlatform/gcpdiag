4a3638373441be7be69694ea08f378a3117fbfe9
David Schweikert <dwes@google.com>
2021-06-01 10:30:50 +0200

    improve caching to be faster and less memory-hungry
    The new cache.cached_api_call decorator especially designed to cache API
    calls:
    
    - uses diskcache so that the memory footprint doesn't grow
      uncontrollably (the API results might be big).
    
    - uses a lock so that if the function is called from two threads
      simultaneously, only one API call will be done and the other will wait
      until the result is available in the cache.
    
    This shrunk the memory usage from 96MB to 80MB and the execution time
    from 6.2s to 4.2s (measured as minimum runtime with 4 executions).
    
    Fixes: b/189805748
    
    Change-Id: I103c10f1f1a6b2196f8b57c37af613f67406c6ee

--- gcp_doctor/config.py
+++ gcp_doctor/config.py
@@ -12,5 +12,8 @@ API_RETRIES = 2
 # Cache directory for diskcache.
 CACHE_DIR = appdirs.user_cache_dir('gcp-doctor')
 
+# Number of seconds to wait for the gcp_doctor.cache API cache lock to be freed.
+CACHE_LOCK_TIMEOUT = 20
+
 # How long to cache documents that rarely change (e.g. predefined IAM roles).
 STATIC_DOCUMENTS_EXPIRY_SECONDS = 3600 * 24
--- gcp_doctor/queries/kms.py
+++ gcp_doctor/queries/kms.py
@@ -1,12 +1,11 @@
 # Lint as: python3
 """Queries related to GCP Cloud Key Management."""
 
-import functools
 import logging
 
 import googleapiclient.errors
 
-from gcp_doctor import config, models, utils
+from gcp_doctor import cache, config, models, utils
 from gcp_doctor.queries import apis
 from gcp_doctor.utils import GcpApiError
 
@@ -31,26 +30,24 @@ class CryptoKey(models.Resource):
   def is_enabled(self) -> bool:
     return self._resource_data['primary'].get('state') == 'ENABLED'
 
-  def __init__(self, key_name):
-    project_id = utils.get_project_by_res_name(key_name)
+  def __init__(self, project_id, resource_data):
     super().__init__(project_id=project_id)
-
-    kms_api = apis.get_api('cloudkms', 'v1')
-    query = kms_api.projects().locations().keyRings().cryptoKeys().get(
-        name=key_name)
-    logging.info('fetching KMS Key %s in project %s',
-                 utils.extract_value_from_res_name(key_name, 'cryptoKeys'),
-                 project_id)
-
-    try:
-      resource_data = query.execute(num_retries=config.API_RETRIES)
-    except googleapiclient.errors.HttpError as err:
-      raise GcpApiError(err) from err
-
     self._resource_data = resource_data
 
 
-@functools.lru_cache(maxsize=None)
+@cache.cached_api_call
 def get_crypto_key(key_name: str) -> CryptoKey:
   """Get a Crypto Key object by its resource name, caching the result."""
-  return CryptoKey(key_name)
+
+  project_id = utils.get_project_by_res_name(key_name)
+  kms_api = apis.get_api('cloudkms', 'v1')
+  query = kms_api.projects().locations().keyRings().cryptoKeys().get(
+      name=key_name)
+  logging.info('fetching KMS Key %s in project %s',
+               utils.extract_value_from_res_name(key_name, 'cryptoKeys'),
+               project_id)
+  try:
+    resource_data = query.execute(num_retries=config.API_RETRIES)
+  except googleapiclient.errors.HttpError as err:
+    raise GcpApiError(err) from err
+  return CryptoKey(project_id, resource_data)
