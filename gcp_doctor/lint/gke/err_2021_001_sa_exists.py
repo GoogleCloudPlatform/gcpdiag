5f063f114e2ada00e280852cd1debd1c9b92ca1f
Sergey Kanzhelev <skanzhelev@google.com>
2021-06-17 16:07:45 +0000

    created a new rule to check the SA exists and fixed a few issues
    Change-Id: I99eb260d899d0c7602783b8cef73288332b1c845

---
+++ gcp_doctor/lint/gke/err_2021_001_sa_exists.py
@@ -0,0 +1,35 @@
+# Lint as: python3
+"""Service Account used by the cluster exists and not disabled
+
+Disabling or deleting service account used by the nodepool will render
+this nodepool not functional. To fix - restore the default compute account
+or service account that was specified when nodepool was created.
+"""
+
+from gcp_doctor import lint, models
+from gcp_doctor.queries import gke, iam
+
+
+def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
+  # Find all clusters.
+  clusters = gke.get_clusters(context)
+  if not clusters:
+    report.add_skipped(None, 'no clusters found')
+  for _, c in sorted(clusters.items()):
+    # Verify service-account exists for every nodepool.
+    for np in c.nodepools:
+      sa = np.service_account
+
+      # TODO(skanzhelev): this may not work for cross-project service accounts
+      accounts = iam.get_service_accounts(context)
+      if sa in iam.get_service_accounts(context):
+        service_account = accounts[sa]
+        if service_account.disabled:
+          report.add_failed(np, f'service account: {sa}\n is disabled')
+      else:
+        if np.has_default_service_account():
+          report.add_failed(np,
+                            f'default service account: {sa}\n does not exists')
+        else:
+          report.add_failed(np, f'service account: {sa}\n does not exists')
+      report.add_ok(np)
