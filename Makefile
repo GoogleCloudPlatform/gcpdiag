78e70097265657cb91b6d0ea3c341ff4e057a108
David Schweikert <dwes@google.com>
2021-09-03 17:55:29 +0200

    Bump version: 0.40-test -> 0.40
    Change-Id: I183ea11263d26f12463f8d45fce1fabcf57406bd
    GitOrigin-RevId: 88f68aafc8d535715f09f4177fe043730ccac1d4

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.40-test
+VERSION=0.40
 DIST_NAME=gcp-doctor-$(VERSION)
 SHELL=/bin/bash
 
--- gcp_doctor/config.py
+++ gcp_doctor/config.py
@@ -18,7 +18,7 @@
 import appdirs
 
 # gcp-doctor version (not configurable, but useful to have here)
-VERSION = '0.40-test'
+VERSION = '0.40'
 
 # Default number of retries for API Calls.
 API_RETRIES = 10
