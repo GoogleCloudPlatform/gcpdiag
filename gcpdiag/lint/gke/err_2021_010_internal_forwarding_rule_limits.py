a98587b900f69835d697eaf95d344666993f7211
Ruben Vales <rubenvales@google.com>
2021-10-01 11:15:51 +0100

    new rule: clusters are not facing ILB quota issues
    b/193382041
    
    Change-Id: I88d51a561a429d38d0d28f42e5025b518c7690ff

---
+++ gcpdiag/lint/gke/err_2021_010_internal_forwarding_rule_limits.py
@@ -0,0 +1,68 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""Check internal peering forwarding limits which affect GKE.
+
+ILB's creation depends on project quota.
+
+The following log lines are searched in the the k8s cluster logs:
+"INTERNAL_FORWARDING_RULES_WITH_PEERING_LIMITS_EXCEEDED" and "SyncLoadBalancerFailed"
+"""
+
+from gcpdiag import lint, models
+from gcpdiag.lint.gke import util
+from gcpdiag.queries import gke, logs
+
+MATCH_STR_1 = 'INTERNAL_FORWARDING_RULES_WITH_PEERING_LIMITS_EXCEEDED'
+MATCH_STR_2 = 'SyncLoadBalancerFailed'
+logs_by_project = {}
+
+
+def prepare_rule(context: models.Context):
+  global logs_by_project
+  logs_by_project[context.project_id] = logs.query(
+      project_id=context.project_id,
+      resource_type='k8s_cluster',
+      log_name='log_id("events")',
+      filter_str=
+      f'jsonPayload.message:"{MATCH_STR_1}" AND jsonPayload.reason:"{MATCH_STR_2}"'
+  )
+
+
+def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
+  # Any work to do?
+  clusters = gke.get_clusters(context)
+  if not clusters:
+    report.add_skipped(None, 'no clusters found')
+
+    # Search the logs.
+  def filter_f(log_entry):
+    try:
+      if (MATCH_STR_1 in log_entry['jsonPayload']['message']) and (
+          MATCH_STR_2 in log_entry['jsonPayload']['reason']):
+        return True
+    except KeyError:
+      return False
+
+  bad_cluster = util.gke_logs_find_bad_cluster(context=context,
+                                               logs_by_project=logs_by_project,
+                                               filter_f=filter_f)
+
+  # Create the report.
+  for _, c in sorted(clusters.items()):
+    if c in bad_cluster:
+      report.add_failed(c, 'Quota issues for ILB :\n. ' + bad_cluster[c])
+    else:
+      report.add_ok(c)
