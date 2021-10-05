703bf8196d60cad8cea0905dfc2c5c65055e50c8
Jack Landau <jlandau@google.com>
2021-10-05 09:52:11 +0000

    Merge "new rule for iam sec make sure a project doesnt have a service account with an owner role"
    GitOrigin-RevId: e09cb531d5dbbfbec27dfeb0b9ac0a7c54927b8b

---
+++ gcpdiag/lint/iam/sec_2021_001_sa_permissions.py
@@ -0,0 +1,41 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""No service accounts have the Owner role
+
+A Service account should not have a role that could potentially increase the security risk
+to the project to malicious activity
+
+See also:
+https://cloud.google.com/iam/docs/best-practices-for-securing-service-accounts
+"""
+
+from gcpdiag import lint, models
+from gcpdiag.queries import crm, iam
+
+ROLE = 'roles/owner'
+
+
+def prefetch_rule(context: models.Context):
+  # Make sure that we have the IAM policy in cache.
+  iam.get_project_policy(context.project_id)
+
+
+def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
+  project = crm.get_project(context.project_id)
+  iam_policy = iam.get_project_policy(context.project_id)
+  roles = iam_policy.get_permissions()
+  for member in roles:
+    if member.startswith('serviceAccount:'):
+      if iam_policy.has_role_permissions(member, ROLE):
+        report.add_failed(project, member + f' has the role {ROLE}')
