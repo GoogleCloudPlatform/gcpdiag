939af80eeba2d41ae7bf7fef11a32ca823e17fc3
David Schweikert <dwes@google.com>
2021-10-08 15:55:17 +0200

    update changelog
    Change-Id: I5c9c0132a97c0e969d250b9a2316f55c2be82dbe
    GitOrigin-RevId: 8239ffe6d010a0a6eb986a41602ae5bfb326452c

--- gcpdiag/config.py
+++ gcpdiag/config.py
@@ -18,7 +18,7 @@
 import appdirs
 
 # gcpdiag version (not configurable, but useful to have here)
-VERSION = '0.46-test'
+VERSION = '0.45-test'
 
 # Default number of retries for API Calls.
 API_RETRIES = 10
