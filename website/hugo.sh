68322a1b1310decc9f04399a7ad3a0acd3fee810
gcpdiag team <noreply@google.com>
2021-10-29 05:36:19 +0000

    Merge "Add podman container runtime support" -- Branch commit log -- commit f29fc9049dc842145a5264cc4543f1bcebc7fddd Author:  gcpdiag team <noreply@google.com> Date:    2021-10-26T17:38:22+09:00
        Add podman container runtime support
    
    - Add podman runtime support to containerized gcpdiag + website (hugo) script
    - Check runtime (cmd) to use based on "SUPPORTED_RUNTIME" - first found
    is used (docker take precedence over podman)
    - For podman set env var 'PODMAN_USERNS=keep-id'
      - Appear needed for podman to work with '-u' option
      - Make file in running container appear as actual user (not root)
    
    Change-Id: Ib55a3ba48d9923a12d824b48794e788ef0436d32
    GitOrigin-RevId: e0509a8548533418ce9e0b26cb7f9f23318e0ac5

--- website/hugo.sh
+++ website/hugo.sh
@@ -1,21 +1,39 @@
 #!/bin/sh
 
 IMAGE=us-docker.pkg.dev/gcpdiag-repo/devel/gcpdiag-hugo:0.1
+SUPPORTED_RUNTIME="docker podman"
 
 if [ "$#" -eq 0 ]; then
-    ARGS="--themesDir=/themes"
+  ARGS="--themesDir=/themes"
 elif [ "$1" = "server" ]; then
-    shift
-    ARGS="server --themesDir=/themes --bind 0.0.0.0 -b http://localhost:1313 $@"
+  shift
+  ARGS="server --themesDir=/themes --bind 0.0.0.0 -b http://localhost:1313 $@"
 else
-    ARGS="$@ --themesDir=/themes"
+  ARGS="$@ --themesDir=/themes"
 fi
 
 mkdir -p "$HOME/.config/gcloud"
 
 [ -t 0 ] && USE_TTY="-it"
 
-docker run $USE_TTY \
+# Set RUNTIME based on available container runtime cmd
+for r in $SUPPORTED_RUNTIME; do
+  if command -v "$r" >/dev/null; then
+    RUNTIME="$r"
+    break
+  fi
+done
+
+if [ -z "$RUNTIME" ]; then
+  >&2 echo "No container runtime found - supported: $SUPPORTED_RUNTIME"
+  exit 1
+fi
+
+if [ "$RUNTIME" = podman ]; then
+  export PODMAN_USERNS=keep-id
+fi
+
+exec "$RUNTIME" run $USE_TTY \
   --rm \
   -u "$(id -u):$(id -g)" \
   -e "USER=$(id -n -u)" \
