ae00455c9c1c76c05bfc1294d83aa82032fc4088
gcpdiag team <noreply@google.com>
2021-11-08 12:50:09 +0000

    Merge "new product: Apigee integration b/193733957" -- Branch commit log -- commit 1d4134e3f649c7811a34b3a9b698e141d734c08d Author:  gcpdiag team <noreply@google.com> Date:    2021-11-05T18:22:33+09:00
        new product: Apigee integration b/193733957
    
    Change-Id: I555bb12725e771878490e2f79a2a519ff45a3378
    GitOrigin-RevId: 6097017932362a345f42d67995b6e4d901e74e97

---
+++ gcpdiag/lint/apigee/warn_2021_001_empty_env.py
@@ -0,0 +1,43 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""Every environment group contains at least one environment.
+
+An environment must be a member of at least one environment group
+before you can access resources defined within it.
+In other words, you must assign an environment to a group before
+you can use it. Or you would receive 404 errors while accessing
+every hostname in the environment group.
+"""
+from gcpdiag import lint, models
+from gcpdiag.queries import apigee
+
+
+def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
+  apigee_org = apigee.get_org(context)
+  if not apigee_org:
+    report.add_skipped(None, 'no Apigee organizations found')
+    return
+  envgroup_list = apigee.get_envgroups(apigee_org[context.project_id])
+  for _, envgroup in envgroup_list.items():
+    environments = apigee.get_envgroups_attachments(envgroup.full_path)
+    if environments:
+      report.add_ok(envgroup)
+    else:
+      report.add_failed(
+          envgroup,
+          f'No environment is attached to the environment group: {envgroup.name}\nAll of the '
+          f'requests to the hostname list below will receive 404 errors: \n{envgroup.host_names}'
+      )
---
+++ gcpdiag/queries/apigee_test.py
@@ -0,0 +1,56 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""Test code in apigee.py."""
+
+from unittest import mock
+
+from gcpdiag import models
+from gcpdiag.queries import apigee, apis_stub
+
+DUMMY_PROJECT_NAME = 'gcpd-apigee-1-lus4'
+DUMMY_APIGEE_ORG_NAME = 'gcpd-apigee-1-lus4'
+DUMMY_APIGEE_ENVGROUP_NAME = 'gcpdiag-demo-envgroup'
+DUMMY_APIGEE_ENVGROUP_FULL_PATH = \
+        f'organizations/{DUMMY_APIGEE_ORG_NAME}/envgroups/{DUMMY_APIGEE_ENVGROUP_NAME}'
+DUMMY_APIGEE_ENVGROUP_HOST_NAMES = ['gcpdiag.apigee.example.com']
+DUMMY_APIGEE_ENVGROUP1_ATTACHMENTS_ENV = 'gcpdiag-demo-env-1'
+DUMMY_APIGEE_ENVGROUP1_NAME = 'gcpdiag-demo-envgroup-1'
+DUMMY_APIGEE_ENVGROUP1_FULL_PATH = \
+       f'organizations/{DUMMY_APIGEE_ORG_NAME}/envgroups/{DUMMY_APIGEE_ENVGROUP1_NAME}'
+
+
+@mock.patch('gcpdiag.queries.apis.get_api', new=apis_stub.get_api_stub)
+class TestOrganization:
+  """Test apigee.Organization."""
+
+  def test_get_org(self):
+    """ get_org returns the right apigee organization by project name """
+    context = models.Context(project_id=DUMMY_PROJECT_NAME)
+    apigee_org = apigee.get_org(context=context)
+    assert DUMMY_APIGEE_ORG_NAME in apigee_org and len(apigee_org) == 1
+
+  def test_get_envgroups(self):
+    apigee_envgroups = apigee.get_envgroups(org_name=DUMMY_APIGEE_ORG_NAME)
+    e = apigee_envgroups[DUMMY_APIGEE_ENVGROUP_NAME]
+    assert len(apigee_envgroups) == 2
+    assert DUMMY_APIGEE_ENVGROUP_NAME in apigee_envgroups
+    assert e.full_path == DUMMY_APIGEE_ENVGROUP_FULL_PATH
+    assert e.host_names == DUMMY_APIGEE_ENVGROUP_HOST_NAMES
+
+  def test_get_envgroups_attachments(self):
+    apigee_envgroups_attachments = apigee.get_envgroups_attachments(
+        envgroup_name=DUMMY_APIGEE_ENVGROUP1_FULL_PATH)
+    assert DUMMY_APIGEE_ENVGROUP1_ATTACHMENTS_ENV in apigee_envgroups_attachments
