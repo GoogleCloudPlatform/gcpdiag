d2ba2f1b6409a50815f9db901c1d3465a2633760
Dawid Malinowski <dawidmalina@google.com>
2022-01-13 14:12:57 +0100

    new gke ERR rule: verify connectivity: gke-[cluster-name]-[cluster-hash]-all
    Change-Id: Ia98f2ee3724d7c5ba9565e91a8a050040030956f

--- test-data/gke1/gke2.tf
+++ test-data/gke1/gke2.tf
@@ -97,8 +97,49 @@ resource "google_container_cluster" "gke2" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
+    tags = ["gke-gke2-custom-node"]
   }
   workload_identity_config {
     workload_pool = "${google_project.project.project_id}.svc.id.goog"
   }
+}
+
+# firewall configuration used for connectivity testing
+
+resource "google_compute_firewall" "vms_test_deny" {
+  name    = "gke-gke2-vms-test-deny"
+  network = "default"
+  project = google_project.project.project_id
+
+  priority = 900
+
+  deny {
+    ports    = ["1-1000"]
+    protocol = "udp"
+  }
+
+  source_ranges = ["10.128.0.0/9"]
+
+  target_tags = google_container_cluster.gke2.node_config[0].tags
+
+  depends_on = [google_container_cluster.gke2]
+}
+
+resource "google_compute_firewall" "all_test_deny" {
+  name    = "gke-gke2-all-test-deny"
+  network = "default"
+  project = google_project.project.project_id
+
+  priority = 900
+
+  deny {
+    ports    = ["1-1000"]
+    protocol = "tcp"
+  }
+
+  source_ranges = [google_container_cluster.gke2.cluster_ipv4_cidr]
+
+  target_tags = google_container_cluster.gke2.node_config[0].tags
+
+  depends_on = [google_container_cluster.gke2]
 }
