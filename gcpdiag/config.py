6d5fad668a95575429b3580dad4be8bcf467581e
David Schweikert <dwes@google.com>
2022-02-11 16:46:09 +0100

    Bump version: 0.52 -> 0.53-test
    Change-Id: I14c41aefc2ac771c2b9d2d6ae1998ad66c2f70f0
    GitOrigin-RevId: 1bf0bf050a23389bd4ceb0c43e6c4bb704bc490e

--- gcpdiag/config.py
+++ gcpdiag/config.py
@@ -18,7 +18,7 @@
 import appdirs
 
 # gcpdiag version (not configurable, but useful to have here)
-VERSION = '0.52'
+VERSION = '0.53-test'
 
 # Default number of retries for API Calls.
 API_RETRIES = 10
