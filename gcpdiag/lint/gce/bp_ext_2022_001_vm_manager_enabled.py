7086d5d08b22993bc8d39164c683f64ac443bd7f
Jack Landau <jlandau@google.com>
2022-02-14 22:55:27 +0000

    BP_EXT: simple rule to check if vm manager is enabled in a project and recommended it to the user
    b/219040572
    
    Change-Id: Icbe23bbbf4e91aef67dc8b27c179545498469988

---
+++ gcpdiag/lint/gce/bp_ext_2022_001_vm_manager_enabled.py
@@ -0,0 +1,32 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""GCP project has VM Manager enabled
+
+Google recommends enabling VM Manager. It provides visibility on software vulnerabilities,
+missing updates and enables to set configuration management policies
+"""
+from gcpdiag import lint, models
+from gcpdiag.queries import apis, crm
+
+
+def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
+  project = crm.get_project(context.project_id)
+  api_list = apis.list_apis(context.project_id)
+  if 'osconfig.googleapis.com' in api_list:
+    report.add_ok(project)
+  else:
+    report.add_failed(project,
+                      'it is recommended to enable VM Manager on your project')
