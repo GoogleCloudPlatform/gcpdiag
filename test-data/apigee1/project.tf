023c5d5e4192b84fae9b1e2383e6cc80ebcc9aaa
Kuba Skiepko <skiepko@google.com>
2022-04-05 14:30:17 +0000

    Merge "Allow to create test projects in org root or folder." -- Branch commit log -- commit bd5719ad786509d02271e1d3394477e541a7c1e9 Author:  Kuba Skiepko <skiepko@google.com> Date:    2022-04-03T11:21:06+02:00
        Allow to create test projects in org root or folder.
    
    Change-Id: Ic00136dab185a9f6573417895893538e43e4dc47
    GitOrigin-RevId: 3a26c2059a2bbf95a8ce0c0b730ca7abd5ec19da

--- test-data/apigee1/project.tf
+++ test-data/apigee1/project.tf
@@ -9,7 +9,8 @@ resource "random_string" "project_id_suffix" {
 resource "google_project" "project" {
   name            = "gcpdiag test - apigee1"
   project_id      = "gcpdiag-apigee1-${random_string.project_id_suffix.id}"
-  org_id          = var.org_id
+  org_id          = var.folder_id == "" ? var.org_id : null
+  folder_id       = var.folder_id != "" ? var.folder_id : null
   billing_account = var.billing_account_id
   skip_delete     = true
 }
