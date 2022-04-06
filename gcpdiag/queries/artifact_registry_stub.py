0ed6cc47c47e0a99b1f3c203b2f490d38469b02b
Kuba Skiepko <skiepko@google.com>
2022-04-06 13:44:47 +0200

    Add iam policy query for Artifact Registry.
    Change-Id: Icf0c80e2c33d268cc66d1a6063038287859522ae
    GitOrigin-RevId: 1c48af7e5445c8b27011882eea75b8dbdb8c5ead

---
+++ gcpdiag/queries/artifact_registry_stub.py
@@ -0,0 +1,63 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""Stub API calls used in gcf.py for testing.
+
+Instead of doing real API calls, we return test JSON data.
+"""
+
+import json
+import re
+
+from gcpdiag.queries import apis_stub
+
+# pylint: disable=unused-argument
+# pylint: disable=invalid-name
+INCORRECT_RESOURCE_ERROR = ('incorrect resource format. Use '
+                            'projects/*/locations/*/repositories/')
+
+
+class ArtifactRegistryApiStub:
+  """Mock object to simulate function api calls."""
+
+  def projects(self):
+    return self
+
+  def locations(self):
+    return self
+
+  def repositories(self):
+    return self
+
+  def getIamPolicy(self, resource: str) -> 'RestCallStub':
+    m = re.match(r'projects/([^/]+)/locations/([^/]+)/repositories/([^/]+)',
+                 resource)
+    if m:
+      project_id = m.group(1)
+      return RestCallStub(project_id, 'artifact-registry-policy.json')
+    else:
+      raise ValueError(INCORRECT_RESOURCE_ERROR)
+
+
+class RestCallStub:
+  """Mock object to simulate executable api request."""
+
+  def __init__(self, project_id: str, json_file: str):
+    self.json_dir = apis_stub.get_json_dir(project_id)
+    self.json_file = json_file
+
+  def execute(self, num_retries: int = 0) -> dict:
+    with open(self.json_dir / self.json_file, encoding='utf-8') as json_file:
+      return json.load(json_file)
