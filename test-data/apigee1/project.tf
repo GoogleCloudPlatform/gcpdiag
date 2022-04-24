3209722679be504d7fbe628eeadaa633ab8bfe01
Feng Liu <felixryu@google.com>
2022-04-24 07:29:58 +0000

    Merge "new rule: verify Apigee Service Agent permissions" -- Branch commit log -- commit 990cfec923f01a16e9a3016d790d2bd6a27b14ff Author:  Feng Liu <felixryu@google.com> Date:    2022-04-18T11:37:29+09:00
        new rule: verify Apigee Service Agent permissions
    
    Change-Id: I77b7ecc82b54664a8f1389dae528d67eb29c78b3
    GitOrigin-RevId: 4295b81316549cf39a77ed46ebd5023b40478a97

--- test-data/apigee1/project.tf
+++ test-data/apigee1/project.tf
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 resource "random_string" "project_id_suffix" {
   length  = 8
   number  = true
@@ -37,4 +53,16 @@ resource "google_project_service" "kms" {
 
 output "project_id" {
   value = google_project.project.project_id
+}
+
+output "project_id_suffix" {
+  value = random_string.project_id_suffix.id
+}
+
+output "project_nr" {
+  value = google_project.project.number
+}
+
+output "org_id" {
+  value = var.org_id
 }
