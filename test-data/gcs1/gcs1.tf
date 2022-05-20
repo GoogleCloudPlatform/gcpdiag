90153bdcb0ab46ac4b017d88e9760ba6195911d7
Kuba Skiepko <skiepko@google.com>
2022-05-20 13:51:31 +0200

    Fix regenerating GCS test data.
    Change-Id: I39fa57bab352cb7b61ad6f15c5e2e778f9370783

--- test-data/gcs1/gcs1.tf
+++ test-data/gcs1/gcs1.tf
@@ -4,3 +4,13 @@ resource "google_storage_bucket" "gcs1bucket" {
   location      = "US"
   storage_class = "STANDARD"
 }
+
+resource "google_storage_bucket" "gcs1bucket-labels" {
+  project       = google_project.project.project_id
+  name          = "gcpdiag-gcs1bucket-labels-${random_string.project_id_suffix.id}"
+  location      = "US"
+  storage_class = "STANDARD"
+  labels = {
+    "label1" = "value1"
+  }
+}
