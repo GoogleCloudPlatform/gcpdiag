07682ec7d7726b717234f1f61366d228314a9dcc
Philipp Shnayder <pschnayder@google.com>
2022-06-01 13:44:51 +0000

    new rule: check for 'response too large' errors
    Change-Id: I7ce0abad5dc521f878b1a2e29a1a8a8f1b0736c2

---
+++ gcpdiag/lint/bigquery/err_2022_002_response_too_large.py
@@ -0,0 +1,55 @@
+#
+# Copyright 2022 Google LLC
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Lint as: python3
+"""BigQuery jobs are not failing due to results being larger than the maximum response size
+
+Query results for SQL queries in BigQuery that generate excessively large results and don't
+set a destination table fail with job error "responseTooLarge"
+"""
+from gcpdiag import lint, models
+from gcpdiag.queries import apis, crm, logs
+
+RESPONSE_TOO_LARGE = [
+    'severity=ERROR',
+    'protoPayload.@type="type.googleapis.com/google.cloud.audit.AuditLog"',
+    'protoPayload.status.message:"Response too large to return"',
+]
+logs_by_project = {}
+
+
+def prepare_rule(context: models.Context):
+  logs_by_project[context.project_id] = logs.query(
+      project_id=context.project_id,
+      resource_type='bigquery_resource',
+      log_name='log_id("cloudaudit.googleapis.com/data_access")',
+      filter_str=' AND '.join(RESPONSE_TOO_LARGE))
+
+
+def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
+  project = crm.get_project(context.project_id)
+  # skip entire rule is logging disabled
+  if not apis.is_enabled(context.project_id, 'logging'):
+    report.add_skipped(project, 'Logging api is disabled')
+    return
+  if not apis.is_enabled(context.project_id, 'bigquery'):
+    report.add_skipped(project, 'bigquery api is disabled')
+    return
+  if logs_by_project.get(context.project_id) and \
+     logs_by_project[context.project_id].entries:
+    report.add_failed(
+        project,
+        'has BigQuery jobs failing because query results are larger than the '
+        'maximum response size')
+  else:
+    report.add_ok(project)
