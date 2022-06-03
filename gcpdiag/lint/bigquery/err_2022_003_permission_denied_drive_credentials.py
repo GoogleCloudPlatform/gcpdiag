15d759b29297b1dcf2e42f32195f6b133a4c9543
Philipp Shnayder <pschnayder@google.com>
2022-06-03 16:24:20 +0000

    new rule: BigQuery failing due to Drive access issue && fixing first two ERR rules to report correctly when report is run fully for BigQuery
    Change-Id: I5d86715449ce40e86d342cb8018e7d15c7260cb2

--- gcpdiag/lint/bigquery/err_2022_003_permission_denied_drive_credentials.py
+++ gcpdiag/lint/bigquery/err_2022_003_permission_denied_drive_credentials.py
@@ -12,22 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # Lint as: python3
-"""BigQuery jobs not failing due to concurrent DML updates on the same table
+"""BigQuery jobs not failing while accessing data in Drive due to a permission issue
 
-Multiple DML queries running concurrently are conflicting with each other.
+BigQuery jobs are failing because the authentication token is missing the Google
+Drive access scope or the user/service account is not granted at least the Viewer
+role on the Drive file.
 """
 from boltons.iterutils import get_path
 
 from gcpdiag import lint, models
 from gcpdiag.queries import apis, crm, logs
 
-MATCH_STR_1 = 'Could not serialize access to table'
-MATCH_STR_2 = 'due to concurrent update'
+MATCH_STR = 'Permission denied while getting Drive credentials'
 
-CONCURRENT_DML_FILTER = [
+MISSING_DRIVE_SCOPE = [
     'severity=ERROR',
     'protoPayload.@type="type.googleapis.com/google.cloud.audit.AuditLog"',
-    f'protoPayload.status.message:("{MATCH_STR_1}" AND "{MATCH_STR_2}")',
+    f'protoPayload.status.message:("{MATCH_STR}")',
 ]
 logs_by_project = {}
 
@@ -37,16 +38,15 @@ def prepare_rule(context: models.Context):
       project_id=context.project_id,
       resource_type='bigquery_resource',
       log_name='log_id("cloudaudit.googleapis.com/data_access")',
-      filter_str=' AND '.join(CONCURRENT_DML_FILTER))
+      filter_str=' AND '.join(MISSING_DRIVE_SCOPE))
 
 
 def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
   project = crm.get_project(context.project_id)
-  # skip entire rule if logging is disabled
+  # skip entire rule is logging disabled
   if not apis.is_enabled(context.project_id, 'logging'):
     report.add_skipped(project, 'Logging api is disabled')
     return
-  # skip entire rule if the BigQuery API is disabled
   if not apis.is_enabled(context.project_id, 'bigquery'):
     report.add_skipped(project, 'bigquery api is disabled')
     return
@@ -54,16 +54,16 @@ def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
      logs_by_project[context.project_id].entries:
     for log_entry in logs_by_project[context.project_id].entries:
       project_ok_flag = True
-      logging_check_path = get_path(log_entry,
-                                    ('protoPayload', 'status', 'message'),
-                                    default='')
-      if ((MATCH_STR_1 not in logging_check_path) or
-          (MATCH_STR_2 not in logging_check_path)):
+      if MATCH_STR not in get_path(log_entry,
+                                   ('protoPayload', 'status', 'message'),
+                                   default=''):
         continue
       else:
         report.add_failed(
             project,
-            'has BigQuery jobs failing due to concurrent DML updates to table')
+            'has BigQuery jobs failing due to issues while accessing a file in Drive '
+            'due to missing permissions on the file or missing Drive access scope'
+        )
         project_ok_flag = False
         break
     if project_ok_flag:
