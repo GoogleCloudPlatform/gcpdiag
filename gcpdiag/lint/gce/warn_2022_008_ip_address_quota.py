7ceb51e3bf9da136f46d7a5332bab3bb827175fb
Wanlong Chen <chenwanlong@google.com>
2022-06-08 16:13:28 +0000

    Merge "new rule: GCE: Verify quota usage for External IP addresses" -- Branch commit log -- commit 61a31338b578e03ef0a52d9d8fb7350fd7057ae3 Author:  Wanlong CHEN <chenwanlong@google.com> Date:    2022-05-27T22:24:17+08:00
        new rule: GCE: Verify quota usage for External IP addresses
    
    Fixes=223753744
    
    Change-Id: Iefd6981117ba6bef427a55d185ce71c150ea8cd4
    GitOrigin-RevId: 3367cd684e1a706be120beb999e4f88690a60d89

---
+++ gcpdiag/lint/gce/warn_2022_008_ip_address_quota.py
@@ -0,0 +1,103 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""GCE External IP addresses quota is not near the limit.
+
+Regional IP quota is for assigning IPv4 addresses to VMs in that region.
+Global IP quota is for assigning IPv4 addresses to global networking resources
+such as load balancers.
+
+Rule will start failing if quota usage will be higher then configured threshold (80%).
+"""
+
+from typing import Dict
+
+from gcpdiag import config, lint, models
+from gcpdiag.queries import gce, monitoring, quotas
+
+GCE_SERVICE_NAME = 'compute.googleapis.com'
+# name of the quota limit
+QUOTA_LIMIT_NAME = '.*-ADDRESSES-per-project.*'
+# name of the quota metric
+QUOTA_METRIC_NAMES = (
+    'compute.googleapis.com/regional_in_use_addresses',
+    'compute.googleapis.com/global_in_use_addresses',
+    'compute.googleapis.com/regional_static_addresses',
+    'compute.googleapis.com/global_static_addresses',
+    'compute.googleapis.com/regional_static_byoip_addresses',
+    'compute.googleapis.com/global_static_byoip_addresses',
+)
+# percentage of the quota limit usage
+QUOTA_LIMIT_THRESHOLD = 0.80
+_query_results_per_project_id: Dict[str, monitoring.TimeSeriesCollection] = {}
+
+
+def prefetch_rule(context: models.Context):
+
+  # fetch the metrics if we have any instances
+  regions_with_instances = gce.get_regions_with_instances(context)
+  if not regions_with_instances:
+    return
+
+  params = {
+      'service_name': GCE_SERVICE_NAME,
+      'limit_name': QUOTA_LIMIT_NAME,
+      'within_days': config.get('within_days')
+  }
+
+  _query_results_per_project_id[context.project_id] = \
+      monitoring.query(
+          context.project_id,
+          quotas.CONSUMER_QUOTA_QUERY_TEMPLATE.format_map(params))
+
+
+def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
+  regions_with_instances = gce.get_regions_with_instances(context)
+  if not regions_with_instances:
+    report.add_skipped(None, 'no instances found')
+    return
+
+  for region in sorted(regions_with_instances, key=lambda r: r.name):
+    all_skipped = True
+    for quota_metric_name in QUOTA_METRIC_NAMES:
+
+      ts_key = frozenset({
+          f'resource.project_id:{context.project_id}',
+          f'metric.quota_metric:{quota_metric_name}',
+          f'resource.location:{region.name}'
+      })
+      try:
+        ts = _query_results_per_project_id[context.project_id][ts_key]
+        all_skipped = False
+      except KeyError:
+        # silently skip
+        continue
+
+      # did we exceeded threshold on any day?
+      exceeded = False
+      for day_value in ts['values']:
+        ratio = day_value[0]
+        limit = day_value[1]
+        if ratio > QUOTA_LIMIT_THRESHOLD:
+          exceeded = True
+
+      if exceeded:
+        report.add_failed(region,
+                          (f'Region has reached {ratio:.0%} of {limit} limit:\n'
+                           f' quota metric: {quota_metric_name}'))
+      else:
+        report.add_ok(region, f' quota metric: {quota_metric_name}')
+
+    # report skip if all data for region not available
+    if all_skipped:
+      report.add_skipped(region, 'no data')
