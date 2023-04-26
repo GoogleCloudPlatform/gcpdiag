612b6a081f59581aae78718400d8747e15d699e6
Ebenezer Ejukorlem <joekorlems@google.com>
2023-04-26 09:44:57 +0000

    Merge "new rule: Check that cloudCDN is enabled on backend services as recommended by performance best practices" -- Branch commit log -- commit 130f72b9090d80a1d20fa89adf94bb1e03ac2e40 Author:  Ebenezer Ejukorlem <joekorlems@google.com> Date:    2023-03-26T18:40:51Z
        new rule: Check that cloudCDN is enabled on backend services as recommended by performance best practices
    
    Change-Id: Ic7c52fd2a9d6aa5684c03fd266e979875bacbfea
    GitOrigin-RevId: 537cfb06cccb43bdfdde2cdd636fbcd732e64f03

---
+++ gcpdiag/lint/lb/bp_2023_001_cloudcdn_for_lb_backend_services.py
@@ -0,0 +1,48 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Cloud CDN is enabled on backends for global external load balancers
+
+Performance best practices recommend that CloudCDN is enabled on backend services
+for Global External HTTP(S) load balancers.
+"""
+
+from gcpdiag import lint, models
+from gcpdiag.queries import lb
+
+EXTERNAL_MANAGED = 'EXTERNAL_MANAGED'
+MAX_BACKENDSERVICES_TO_REPORT = 10
+
+
+def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
+
+  bs_list = lb.get_backend_services(context.project_id)
+
+  # return if there are no BackendServices found in the project
+  if not bs_list:
+    report.add_skipped(None, 'no backend services found')
+    return
+
+  for bs in bs_list:
+    # fail for backend services for Global HTTP(S) LB which have CloudCDN enabled
+    if (bs.load_balancing_scheme == EXTERNAL_MANAGED and
+        not bs.is_enable_cdn and bs.region == 'None'):
+      report.add_failed(bs)
+
+    # pass for backend services for Global HTTP(S) LB which have CloudCDN enabled
+    elif (bs.load_balancing_scheme == EXTERNAL_MANAGED and bs.is_enable_cdn and
+          bs.region == 'None'):
+      report.add_ok(bs)
+    else:
+      # skip for non-global HTTP(S) LB backend services
+      report.add_skipped(bs, 'Non Global HTTP(S) Load balancer backend service')
