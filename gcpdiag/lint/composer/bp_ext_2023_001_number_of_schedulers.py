82428b7e4ea8e536d4081a421b3fe3459fc05013
Junggil Lee <junggill@google.com>
2023-06-02 09:00:52 +0000

    Merge "new rule: Cloud Composer has no more than 2 Airflow schedulers" -- Branch commit log -- commit c807750dd452da2d0495005e12f0719344c54acc Author:  Junggil Lee <junggill@google.com> Date:    2023-05-22T14:38:30+09:00
        new rule: Cloud Composer has no more than 2 Airflow schedulers
    
    Change-Id: I601fb377d71c47b0f8c4adcb0f28d472b72c071c
    GitOrigin-RevId: a7021cb8586c11081b78b73b893fcea1241c94e6

---
+++ gcpdiag/lint/composer/bp_ext_2023_001_number_of_schedulers.py
@@ -0,0 +1,42 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Cloud Composer has no more than 2 Airflow schedulers
+
+In general, extra schedulers more than 2 consumes resources of your environment
+without contributing to overall performance. We recommend starting with two
+schedulers and then monitoring the performance of your environment.
+"""
+
+from gcpdiag import lint, models
+from gcpdiag.queries import apis, composer
+
+
+def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
+  if not apis.is_enabled(context.project_id, 'composer'):
+    report.add_skipped(None, 'composer is disabled')
+    return
+
+  envs = composer.get_environments(context)
+
+  if not envs:
+    report.add_skipped(None, 'no Cloud Composer environments found')
+    return
+
+  for env in envs:
+    if env.num_schedulers > 2:
+      report.add_failed(
+          env, f'{env.name} is configured more than 2 Airflow schedulers'
+          ' (number_of_schedulers: {env.num_schedulers})')
+    else:
+      report.add_ok(env)
