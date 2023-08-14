7afe00292b1ff264241fc915bc4bd1e0d51a4287
Rodney Peck <ropeck@google.com>
2023-08-14 13:17:22 -0700

    Bump version: 0.64 -> 0.65-test
    Change-Id: I267ec83b1353cdee2701af92bed68d47e44f4561
    GitOrigin-RevId: 179a6b3ff82e9061aa79b1e8967598ca11b407c6

--- gcpdiag/config.py
+++ gcpdiag/config.py
@@ -23,7 +23,7 @@ import appdirs
 import yaml
 
 # gcpdiag version (not configurable, but useful to have here)
-VERSION = '0.64'
+VERSION = '0.65-test'
 """
 Configuration properties are divided into 3 main categories:
 - static (class properties) which values cannot be changed or provided
