8c237d018a418d902f041924704e0a34c850b9e2
Greg Bray <greg.bray@gmail.com>
2023-08-21 20:59:18 -0600

    fixup pre-commit errors

--- gcpdiag/lint/gke/err_2023_011_wi_pod_ip_not_found.py
+++ gcpdiag/lint/gke/err_2023_011_wi_pod_ip_not_found.py
@@ -20,7 +20,7 @@ a misconfiguration or a workload that is not compatible with GKE Workload Identi
 
 from gcpdiag import lint, models
 from gcpdiag.lint.gke import util
-from gcpdiag.queries import gke, gce, crm, logs
+from gcpdiag.queries import crm, gce, gke, logs
 
 GKE_METADATA_SERVER_CONTAINER_NAME = 'gke-metadata-server'
 MATCH_STR = 'Unable to find pod: generic::not_found: retry budget exhausted'
@@ -29,10 +29,14 @@ MATCH_STR_END = 'not recorded in the snapshot'
 # https://cloud.google.com/container-optimized-os/docs/how-to/logging
 GCE_METADATA_COS_LOGGING_ENABLED = 'google-logging-enabled'
 GCE_METADATA_COS_FLUENT_BIT = 'google-logging-use-fluentbit'
-COS_LEGACY_AGENT_WARNING = 'Project metadata contains google-logging-enabled=true but not google-logging-use-fluentbit=true\nCOS VMs before Milestone 109 will use the deprecated legacy logging agent, which is not compatible with GKE Workload Identity. See https://cloud.google.com/container-optimized-os/docs/how-to/logging'
+COS_LEGACY_AGENT_WARNING = """Project metadata contains google-logging-enabled=true but not
+google-logging-use-fluentbit=true. COS VMs before Milestone 109 will use the deprecated
+legacy logging agent, which is not compatible with GKE Workload Identity.
+See https://cloud.google.com/container-optimized-os/docs/how-to/logging"""
 
 # k8s_container
 metadata_server_errors = {}
+
 
 def prepare_rule(context: models.Context):
   metadata_server_errors[context.project_id] = logs.query(
@@ -53,12 +57,14 @@ def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
   project_metadata = gce.get_project_metadata(context.project_id)
   if project_metadata.get(GCE_METADATA_COS_LOGGING_ENABLED) == 'true' and \
     not project_metadata.get(GCE_METADATA_COS_FLUENT_BIT) == 'true':
-    report.add_failed(crm.get_project(context.project_id), COS_LEGACY_AGENT_WARNING)
+    report.add_failed(crm.get_project(context.project_id),
+                      COS_LEGACY_AGENT_WARNING)
 
   # Search the logs.
   def filter_f(log_entry):
     try:
-      if log_entry['jsonPayload']['message'].endswith(MATCH_STR_END) and MATCH_STR in log_entry['jsonPayload']['message']:
+      if log_entry['jsonPayload']['message'].endswith(
+          MATCH_STR_END) and MATCH_STR in log_entry['jsonPayload']['message']:
         return True
     except KeyError:
       return False
