385309236c93638428ccbc075c691ac7c9fa3ba1
Ian Mburu <mburuian@google.com>
2024-01-08 10:05:38 +0000

    Merge "new rule: pubsub push subscriptions have no push errors" -- Branch commit log -- commit 952743c67be25dddb60f993bdf3e8350b5185c6e Author:  Ian Mburu <mburuian@google.com> Date:    2024-01-04T16:07:09Z
        new rule: pubsub push subscriptions have no push errors
    
    Change-Id: I560a3187f19a93027580e9b600ce6663046f46fa
    GitOrigin-RevId: ec7391835c7e58b2d46ec96594f4a3c4ac48d5ec

---
+++ gcpdiag/lint/pubsub/warn_2023_006_push_requests_failing.py
@@ -0,0 +1,97 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""Push delivery requests for push subscriptions are not failing.
+
+For any push subscription, delivery to the endpoint should return an ack
+response for successfully processed messages.
+"""
+
+from itertools import islice
+from typing import Dict
+
+from boltons.iterutils import get_path
+
+from gcpdiag import lint, models
+from gcpdiag.queries import crm, monitoring, pubsub
+
+push_request_count: Dict[str, monitoring.TimeSeriesCollection] = {}
+DURATION = '10m'  # 10 minutes - also used to measure delivery latency health
+MAX_SUBSCRIPTIONS_TO_DISPLAY = 10
+
+
+def prefetch_rule(context: models.Context):
+  subscription_name = ''
+  query_push_request_count = (
+      'fetch pubsub_subscription| metric'
+      ' "pubsub.googleapis.com/subscription/push_request_count"| filter'
+      ' resource.project_id == "{}" &&'
+      ' (resource.subscription_id == "{}") | align rate(1m)|'
+      ' every 1m| group_by'
+      ' [metric.response_class],[value_push_request_count_aggregate:'
+      ' aggregate(value.push_request_count)] | within {}')
+
+  subscriptions = pubsub.get_subscriptions(context)
+
+  for _, subscription in subscriptions.items():
+    if subscription.is_push_subscription():
+      subscription_name = subscription.name
+      push_request_count[subscription_name] = monitoring.query(
+          context.project_id,
+          query_push_request_count.format(context.project_id, subscription_name,
+                                          DURATION),
+      )
+
+
+def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
+  project = crm.get_project(context.project_id)
+
+  if not push_request_count:
+    report.add_skipped(None, 'no subscription metrics found')
+
+  failing_subscriptions = set()
+  for subscription_name, push_metric in push_request_count.items():
+    if (not push_metric
+       ):  # empty for subscription without traffic for the duration
+      continue
+    else:
+      for metric_values in push_metric.values():
+        # one of ['ack', 'deadline_exceeded', 'internal', 'invalid',
+        # 'remote_server_4xx', 'remote_server_5xx', 'unreachable']
+        response_class = get_path(metric_values,
+                                  ('labels', 'metric.response_class'))
+
+        if response_class != 'ack':
+          failing_subscriptions.add(subscription_name)
+
+  # reporting
+  if failing_subscriptions:
+    extra_subscriptions = ''
+    if len(failing_subscriptions) > MAX_SUBSCRIPTIONS_TO_DISPLAY:
+      extra_subscriptions = (
+          ', and'
+          f' {len(failing_subscriptions) - MAX_SUBSCRIPTIONS_TO_DISPLAY} more'
+          ' subscriptions')
+
+  # pylint: disable=line-too-long
+    report.add_failed(
+        project,
+        f'{len(failing_subscriptions)} subscriptions have non-ack responses'
+        ' from the endpoint:'
+        f' {", ".join(islice(failing_subscriptions, MAX_SUBSCRIPTIONS_TO_DISPLAY))}{extra_subscriptions}',
+    )
+  # pylint: enable=line-too-long
+  else:
+    report.add_ok(project)
