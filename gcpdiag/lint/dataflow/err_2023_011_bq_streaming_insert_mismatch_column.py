ba520e06b0d8f8f6eec035b34ea5ba1b7c70c36b
gcpdiag team <noreply@google.com>
2024-01-17 11:21:54 +0000

    Merge "mismatched column type commit" -- Branch commit log -- commit 0d7ce14544e41ae5ca928fe4429266a93a749c7e Author:  gcpdiag team <noreply@google.com> Date:    2023-12-25T15:29:12Z
        mismatched column type commit
    
    Change-Id: I151e119357c52ee5f8eaffa3e69596e74f633f07
    GitOrigin-RevId: 58a0e50f87e7db816db5a969a4067257600f1a73

---
+++ gcpdiag/lint/dataflow/err_2023_011_bq_streaming_insert_mismatch_column.py
@@ -0,0 +1,94 @@
+#
+# Copyright 2023 Google LLC
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+# pylint: disable=line-too-long
+"""Dataflow job using streaming insert did not fail due to mismatched column type.
+
+The Dataflow job writing to bigquery using streaming inserts can fail due to mismatched column type.
+"""
+
+from itertools import islice
+
+from boltons.iterutils import get_path
+
+from gcpdiag import lint, models
+from gcpdiag.queries import apis, crm, logs
+
+MATCH_STRINGS = [
+    ' java.io.IOException: Insert failed:', 'Insert failed',
+    'Cannot convert value to integer (bad value):str_for_int_column'
+]
+LOG_FILTER = [
+    'severity=ERROR',
+    'jsonPayload.message: ("{}")'.format('" AND "'.join(MATCH_STRINGS)),
+]
+
+project_logs = {}
+MAX_JOBS_TO_DISPLAY = 10
+
+
+def prepare_rule(context: models.Context):
+  project_id = context.project_id
+  log_name = 'log_id("dataflow.googleapis.com/worker")'
+  project_logs[project_id] = logs.query(
+      project_id=project_id,
+      resource_type='dataflow_step',
+      log_name=log_name,
+      filter_str=' AND '.join(LOG_FILTER))  # "returns LogsQuery object"
+
+
+def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
+  """Check that no (dataflow) job failed due to mismatched column type error."""
+  project = crm.get_project(context.project_id)
+
+  # skip entire rule if logging is disabled
+  if not apis.is_enabled(context.project_id, 'logging'):
+    report.add_skipped(project, 'logging api is disabled')
+    return
+
+  # skip entire rule if dataflow API is disabled
+  if not apis.is_enabled(context.project_id, 'dataflow'):
+    report.add_skipped(project, 'dataflow api is disabled')
+    return
+
+  if (context.project_id in project_logs and
+      project_logs[context.project_id].entries):
+    failed_jobs = set()
+    for log_entry in project_logs[context.project_id].entries:
+      current_entry = get_path(log_entry, ('jsonPayload', 'message'), '')
+
+      if log_entry['severity'] != 'ERROR' or all(
+          m not in current_entry for m in MATCH_STRINGS):
+        continue
+
+      job_id = get_path(log_entry, ('resource', 'labels', 'job_id'))
+      failed_jobs.add(job_id)
+
+    if failed_jobs:
+      extra_jobs = (f', and {len(failed_jobs) - MAX_JOBS_TO_DISPLAY} more jobs'
+                    if len(failed_jobs) > MAX_JOBS_TO_DISPLAY else '')
+
+      report.add_failed(
+          project,
+          f'{len(failed_jobs)} job(s) failed due to mismatched column type'
+          f' errors: {", ".join(islice(failed_jobs, MAX_JOBS_TO_DISPLAY))} {extra_jobs}',
+      )
+    else:
+      # only irrelevant logs were fetched
+      report.add_ok(project)
+
+  else:
+    # no logs matched the filter
+    report.add_ok(project)
