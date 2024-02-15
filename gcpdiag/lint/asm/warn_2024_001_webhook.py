d2793849dca4bea803f92add1acc828991020b06
gcpdiag team <noreply@google.com>
2024-02-15 05:02:14 +0000

    Merge "new rule: Webhook failed" -- Branch commit log -- commit 944afff2a4f1340f39d7d919e721304207a7388f Author:  gcpdiag team <noreply@google.com> Date:    2024-01-26T08:39:31Z
        new rule: Webhook failed
    
    Change-Id: I59e230e26096c8526322079dc2ac71b530d064df
    GitOrigin-RevId: edafcfa0c45d1273ad72f4581e2c72f5d3b7f3c0

---
+++ gcpdiag/lint/asm/warn_2024_001_webhook.py
@@ -0,0 +1,70 @@
+#Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""No webhook creation failures were found.
+
+Error calling webhook namespace.sidecar-injector.istio.io. Make sure endpoints
+are present for the service.
+
+"""
+
+from gcpdiag import lint, models
+from gcpdiag.lint.gke import util
+from gcpdiag.queries import apis, gke, logs
+
+MATCH_STR_1 = 'Error creating: Internal error occurred: failed calling webhook '
+MATCH_STR_2 = 'namespace.sidecar-injector.istio.io'
+MATCH_STR_3 = 'no endpoints available for service'
+
+logs_by_project = {}
+
+
+def prepare_rule(context: models.Context):
+  clusters = gke.get_clusters(context)
+  for project_id in {c.project_id for c in clusters.values()}:
+    logs_by_project[project_id] = logs.query(
+        project_id=project_id,
+        resource_type='k8s_cluster',
+        log_name='log_id("events")',
+        filter_str=(f'jsonPayload.message=~"{MATCH_STR_1}" AND '
+                    f'jsonPayload.message=~"{MATCH_STR_2}" AND '
+                    f'jsonPayload.message=~"{MATCH_STR_3}"'))
+
+
+def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
+  # skip entire rule is logging disabled
+  if not apis.is_enabled(context.project_id, 'logging'):
+    report.add_skipped(None, 'logging api is disabled')
+    return
+  # Any work to do?
+  clusters = gke.get_clusters(context)
+  if not clusters:
+    report.add_skipped(None, 'no clusters found')
+    # Search the logs.
+  def filter_f(log_entry):
+    try:
+      if MATCH_STR_1 in log_entry['jsonPayload']['message']:
+        if MATCH_STR_2 in log_entry['jsonPayload']['message']:
+          if MATCH_STR_3 in log_entry['jsonPayload']['message']:
+            return True
+    except KeyError:
+      return False
+
+  bad_clusters = util.gke_logs_find_bad_clusters(
+      context=context, logs_by_project=logs_by_project, filter_f=filter_f)
+  # Create the report.
+  for _, c in sorted(clusters.items()):
+    if c in bad_clusters:
+      report.add_failed(c, logs.format_log_entry(bad_clusters[c]))
+    else:
+      report.add_ok(c)
