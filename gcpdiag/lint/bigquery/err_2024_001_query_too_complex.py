47f1e322e5605cad439aa46ebadb3385f72663c8
Surmay Bhavsar <surmayb@google.com>
2024-03-30 12:54:06 +0000

    new rule : bigquery query is too complex
    Bug-Id: b/244139045
    
    Change-Id: I4adfe21001927b632168f194f84646c57eddf210
    GitOrigin-RevId: d2b7207e501556463e4df8d5451735648c93319a

---
+++ gcpdiag/lint/bigquery/err_2024_001_query_too_complex.py
@@ -0,0 +1,78 @@
+#
+# Copyright 2024 Google LLC
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Lint as: python3
+"""BigQuery jobs are not failing due to too many subqueries or query is too complex
+
+The query is failing with Resources exceeded during query execution. This
+usually happens when VIEWs, WITH clauses and SQL UDFs are inlined/expanded in
+query and then we measure query complexity. Nested inlining could cause
+exponential growth in complexity,Sometimes customers use WITH clause as
+substitution for temp tables. It is not a good practie and they could use
+scripts and temp tables instead.
+"""
+from boltons.iterutils import get_path
+
+from gcpdiag import lint, models
+from gcpdiag.queries import apis, crm, logs
+
+MATCH_STR = (
+    'Not enough resources for query planning - too many subqueries or query is'
+    ' too complex')
+
+QUERY_COMPLEX = [
+    'severity=ERROR',
+    'protoPayload.@type="type.googleapis.com/google.cloud.audit.AuditLog"',
+    f'protoPayload.status.message:("{MATCH_STR}")',
+]
+logs_by_project = {}
+
+
+def prepare_rule(context: models.Context):
+  logs_by_project[context.project_id] = logs.query(
+      project_id=context.project_id,
+      resource_type='bigquery_resource',
+      log_name='log_id("cloudaudit.googleapis.com/data_access")',
+      filter_str=' AND '.join(QUERY_COMPLEX),
+  )
+
+
+def run_rule(context: models.Context, report: lint.LintReportRuleInterface):
+  project = crm.get_project(context.project_id)
+  # skip entire rule is logging disabled
+  if not apis.is_enabled(context.project_id, 'logging'):
+    report.add_skipped(project, 'Logging api is disabled')
+    return
+  if not apis.is_enabled(context.project_id, 'bigquery'):
+    report.add_skipped(project, 'BigQuery api is disabled')
+    return
+  if (logs_by_project.get(context.project_id) and
+      logs_by_project[context.project_id].entries):
+    for log_entry in logs_by_project[context.project_id].entries:
+      project_ok_flag = True
+      if MATCH_STR not in get_path(log_entry,
+                                   ('protoPayload', 'status', 'message'),
+                                   default=''):
+        continue
+      else:
+        report.add_failed(
+            project,
+            'has BigQuery jobs failing due to high complexity. Please optimize'
+            ' the query',
+        )
+        project_ok_flag = False
+        break
+    if project_ok_flag:
+      report.add_ok(project)
+  else:
+    report.add_ok(project)
