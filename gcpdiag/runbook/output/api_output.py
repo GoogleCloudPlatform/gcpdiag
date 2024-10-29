79165676e8fc8ac131d76c5ee9b5a04e1437b551
Joel Jacob <joeljacob@google.com>
2024-10-28 21:19:53 -0400

    Fixes: 371970441 Return logs when gcpdiag is executed via API service
    Change-Id: I1c9afd3c348b313e9f9d80de8cfc46a86f837330
    GitOrigin-RevId: c5128235056089d57dc4de21aad6164ce8b05eaa

--- gcpdiag/runbook/output/api_output.py
+++ gcpdiag/runbook/output/api_output.py
@@ -11,10 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" API class for api output implementations """
+"""API class for api output implementations"""
+
+import logging
 
 from gcpdiag.runbook.output.base_output import BaseOutput
 
 
 class ApiOutput(BaseOutput):
-  pass
+  """API output implementation."""
+
+  def get_logging_handler(self) -> logging.Handler:
+    stream_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(levelname)-6s: %(message)s')
+    stream_handler.setFormatter(formatter)
+    return stream_handler
